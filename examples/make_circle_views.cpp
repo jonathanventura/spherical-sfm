d598f87c8819513e8870ed37696872bdcb751b06
Jonathan Ventura <jventu09@calpoly.edu>
2025-11-14 14:48:53 -0800

    adding missing file; update cmake for stitcher apps

---
+++ examples/make_circle_views.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+
+#include <gflags/gflags.h>
+
+#include "stereo_panorama_tools.h"
+
+using namespace sphericalsfm;
+using namespace stereopanotools;
+
+DEFINE_string(intrinsics, "", "Path to intrinsics (focal centerx centery)");
+DEFINE_string(video, "", "Path to video or image search pattern like frame%06d.png");
+DEFINE_string(output, "", "Path to output directory");
+DEFINE_int32(numviews, 32, "Number of views");
+DEFINE_bool(loop, true, "Trajectory is a closed loop");
+
+int main( int argc, char **argv )
+{
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    
+    double focal, centerx, centery;
+    std::ifstream intrinsicsf( FLAGS_intrinsics );
+    intrinsicsf >> focal >> centerx >> centery;
+    
+    std::cout << "intrinsics : " << focal << ", " << centerx << ", " << centery << "\n";
+
+    Intrinsics intrinsics(focal,centerx,centery);
+    make_circle_views( intrinsics, FLAGS_video, FLAGS_output, FLAGS_numviews, FLAGS_loop );
+}
